887ae77561b68b38ae0e8f7aed0cb3141b3c6ba7
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-06-17 17:05:20 +0300

    travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -12,17 +12,17 @@ function repair_wheel {
 
 
 # Install a system package required by our library
-yum install -y atlas-devel
+#yum install -y atlas-devel
 
 # Compile wheels
-/opt/python/cp35-35mu/pip install -r /io/dev-requirements.txt
-/opt/python/cp35-35mu/pip wheel /io/ --no-deps -w wheelhouse/
+/opt/python/cp35-35mu/bin/pip install -r /io/dev-requirements.txt
+/opt/python/cp35-35mu/bin/pip wheel /io/ --no-deps -w wheelhouse/
 
-/opt/python/cp36-36mu/pip install -r /io/dev-requirements.txt
-/opt/python/cp36-36mu/pip wheel /io/ --no-deps -w wheelhouse/
+/opt/python/cp36-36mu/bin/pip install -r /io/dev-requirements.txt
+/opt/python/cp36-36mu/bin/pip wheel /io/ --no-deps -w wheelhouse/
 
-/opt/python/cp37-37mu/pip install -r /io/dev-requirements.txt
-/opt/python/cp37-37mu/pip wheel /io/ --no-deps -w wheelhouse/
+/opt/python/cp37-37mu/bin/pip install -r /io/dev-requirements.txt
+/opt/python/cp37-37mu/bin/pip wheel /io/ --no-deps -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
@@ -31,12 +31,12 @@ done
 
 # Install packages and test
 
-/opt/python/cp35-35mu/pip install python-manylinux-demo --no-index -f /io/wheelhouse
+/opt/python/cp35-35mu/bin/pip install python-manylinux-demo --no-index -f /io/wheelhouse
 (cd "$HOME";  "/opt/python/cp35-35mu/nosetests" pymanylinuxdemo)
 
-/opt/python/cp36-36mu/pip install python-manylinux-demo --no-index -f /io/wheelhouse
+/opt/python/cp36-36mu/bin/pip install python-manylinux-demo --no-index -f /io/wheelhouse
 (cd "$HOME";  "/opt/python/cp36-36mu/nosetests" pymanylinuxdemo)
 
-/opt/python/cp37-37mu/pip install python-manylinux-demo --no-index -f /io/wheelhouse
+/opt/python/cp37-37mu/bin/pip install python-manylinux-demo --no-index -f /io/wheelhouse
 (cd "$HOME";  "/opt/python/cp37-37mu/nosetests" pymanylinuxdemo)
 

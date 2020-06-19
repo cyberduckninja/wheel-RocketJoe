2891d80daa2926f3a3ec8ab0dc07ccbac7d9dac8
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-06-19 21:50:11 +0300

    check conan install

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -21,6 +21,7 @@ ln -s /opt/python/cp36-cp36m/bin/conan /usr/bin/conan
 
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remote add jinncrafters https://api.bintray.com/conan/jinncrafters/conan
+conan install --generator
 conan profile update settings.compiler.libcxx=libstdc++11 default
 
 conan install \

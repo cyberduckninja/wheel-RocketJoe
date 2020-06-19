56808d8c2c1b8c4b12547ba1dc831f6a2b0905f1
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-06-19 22:13:32 +0300

    debug print

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -23,8 +23,7 @@ conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-co
 conan remote add jinncrafters https://api.bintray.com/conan/jinncrafters/conan
 conan profile new default --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
-echo $(pwd)
-echo $(ls -alh)
+echo $(ls)
 conan install \
              -b missing \
              -b boost \
